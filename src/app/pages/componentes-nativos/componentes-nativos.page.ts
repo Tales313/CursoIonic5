7d62f3f9187f2b3b07dc339f6c934d1fae9a9413
Tales <tales.11.98@gmail.com>
2021-01-29 22:19:28 -0300

    aula 39 - tirando fotos + instalando cordova

--- src/app/pages/componentes-nativos/componentes-nativos.page.ts
+++ src/app/pages/componentes-nativos/componentes-nativos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController} from '@ionic/angular';
 
 @Component({
   selector: 'app-componentes-nativos',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentesNativosPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  showPage(url: string) {
+    this.navCtrl.navigateForward(url);
   }
 
 }
